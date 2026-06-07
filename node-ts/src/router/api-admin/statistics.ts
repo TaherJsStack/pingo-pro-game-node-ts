d8295e58eae374e3409c7bb63258127ebf7f4366
Taher Ahmed <Taher.Ahmed@softec.ai>
2026-06-07 23:43:27 +0300

    Enhance statistics API routes with middleware and new endpoint
    - Added `signReqData` and `rootAuthGuard` middleware to the statistics router for improved security and request handling.
    - Introduced a new GET route `/kpi` to fetch platform KPI summaries, expanding the statistics API functionality.
    - This update strengthens the API's security posture and enhances data retrieval capabilities.

--- node-ts/src/router/api-admin/statistics.ts
+++ node-ts/src/router/api-admin/statistics.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
 import {StatisticsController} from '../../controllers/root-api/statistics';
+import signReqData from '../../middleware/sign-req-data';
+import rootAuthGuard from '../../middleware/root-auth.guard';
 
 const router = express.Router();
 const controller = new StatisticsController();
 
+router.use(signReqData, rootAuthGuard);
+
 // Route to get a statistics
 router.get("/aggregate", controller.getAggregate);
 router.get("", controller.getCollectionStatistics);
+router.get("/kpi", controller.getPlatformKpiSummary);
 
 export default router;
