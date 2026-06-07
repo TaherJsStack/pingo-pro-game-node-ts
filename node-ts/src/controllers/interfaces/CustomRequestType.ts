59ed487ca31c89ccfb270632b5eeea4dd107e736
Taher Ahmed <Taher.Ahmed@softec.ai>
2026-06-07 23:20:47 +0300

    Enhance CustomRequestType interface with additional authentication fields
    - Added optional properties `tenantId`, `role`, `email`, `permission`, `authType`, and `permissions` to the `authData` object for improved authentication context.
    - Introduced an `idempotency` object with properties `key`, `route`, `requestHash`, and `recordId` to support idempotent requests, enhancing request handling capabilities.

--- node-ts/src/controllers/interfaces/CustomRequestType.ts
+++ node-ts/src/controllers/interfaces/CustomRequestType.ts
@@ -4,5 +4,17 @@ export interface CreateItemRequest<T> extends Request {
   body: T;
   authData: {
     id: string;
+    tenantId?: string;
+    role?: string;
+    email?: string;
+    permission?: string | number[];
+    authType?: string;
+    permissions?: any;
+  };
+  idempotency?: {
+    key: string;
+    route: string;
+    requestHash: string;
+    recordId: string;
   };
 }
