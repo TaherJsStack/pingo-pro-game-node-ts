02641f18d80ff40ff7974b1f4f3bcf9a8f50fd1c
Taher Ahmed <Taher.Ahmed@softec.ai>
2026-06-11 00:50:27 +0300

    Update ISessionService interface to enhance createItem return type
    - Modified the createItem method to return a more structured Promise type, including an item and a boolean indicating if it was added to an existing session, improving clarity and usability.

--- node-ts/src/services/interfaces/ISessionService.ts
+++ node-ts/src/services/interfaces/ISessionService.ts
@@ -1,6 +1,6 @@
 import { ISession } from '../../models/interfaces/session.interface';
 
 export interface ISessionService {
-  createItem(body: ISession, authUserId: string): Promise<any>;
+  createItem(body: ISession, authUserId: string): Promise<{ item: any; wasAddedToExisting: boolean }>;
   endSession(sessionId: string, body: any, authUserId: string): Promise<{ session: any; bill: any; message: string }>;
 }
